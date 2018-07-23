8bf9bd62f7c4e007b6d087789080885060deb159
Jiecheng Chen <chjchjason@gmail.com>
2018-07-22 21:27:17 -0700

    test1
    test1

--- flea-market-client/src/index.js
+++ flea-market-client/src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Button} from 'antd-mobile'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Button>Test</Button>, document.getElementById('root'))
